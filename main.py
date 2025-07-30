42f89407d53d973b9b38b077e2098f4ab9cbcbc1
dsbojaca <dsbojacar@gmail.com>
2025-07-30 10:49:09 -0500

    cambios para la limpieza de datos

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 from utils.driver import get_driver
 from etl.extract import extract_properties
 from etl.load import load_to_csv
+from etl.transform import clean_data
 
 def main():
     driver = get_driver()
-    data = extract_properties(driver, n=10)
+    data_raw = extract_properties(driver, n=10)
     
-    for i, item in enumerate(data):
+
+    for i, item in enumerate(data_raw):
         print(f"\n📌 Propiedad {i+1}")
         print(f"💰 Precio: {item['Precio']}")
         print(f"🏠 Tipología: {item['Tipología']}")
         print(f"📍 Arrendador: {item['Arrendador']}")
 
+    data= clean_data(data_raw)
     load_to_csv(data)
     driver.quit()
 
